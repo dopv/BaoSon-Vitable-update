5cf8b1ba05ce12000783573378463f8aa9027a7d
Phil Douglas <phil.douglas@2and2.com.au>
2021-01-21 15:37:13 +1100

    added backend server api url to manifest files

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Constants from 'expo-constants';
+const {manifest:{extra:{apiUrl}}} = Constants
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>apiUrl: {apiUrl}</Text>
       <StatusBar style="auto" />
     </View>
   );
